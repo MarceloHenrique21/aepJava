44ac1e5dacc79bdbff25dc8d8e5567fc771e9036
Marcelo Henrique <73535877+feitan02@users.noreply.github.com>
2022-06-13 19:44:17 -0300

    Delete aep directory

--- aep/Main.java
+++
@@ -1,7 +0,0 @@
-import MenuInicial.Inicio;
-public class Main {
-    public static void main(String[] args) {
-           Inicio menuInicial = new Inicio();
-      menuInicial.iniciar();
-    }
-}
--- aep/classesGeral/Produto.java
+++
@@ -1,9 +0,0 @@
-package classesGeral;
-public class Produto {
-    private int Id;
-    public  String nome;
-    public  String descricao;
-    public  float preco;
-    }
-
-
